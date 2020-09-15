764115678fb09ece2d0c1387f20f648cbbf3d8e9
Luo <547605677@qq.com>
2020-09-15 17:46:02 +0800

    增加redis配置

--- src/main/java/com/luocj/springbootplayground/controllers/UserController.java
+++ src/main/java/com/luocj/springbootplayground/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.luocj.springbootplayground.controllers;
 
 import com.luocj.springbootplayground.entities.UserEntity;
 import com.luocj.springbootplayground.repositories.UserRepository;
+import com.luocj.springbootplayground.utils.RedisUtil;
 import com.luocj.springbootplayground.vos.CreateUserVo;
 import com.luocj.springbootplayground.vos.Result;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,13 +11,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+
+    @Resource
+    private RedisUtil redisUtil;
+
     @GetMapping("/user/all")
     public Result getAllUser(){
-        return Result.success(userRepository.findAll());
+        return Result.success(redisUtil.get("users"));
     }
 
     @PostMapping("/user/add")
@@ -24,6 +31,10 @@ public class UserController {
         UserEntity n = new UserEntity();
         n.setName(createUserVo.getName());
         userRepository.save(n);
-        return Result.success(userRepository.findAll());
+
+        redisUtil.set("users",userRepository.findAll());
+        return Result.success(redisUtil.get("users"));
     }
+
+
 }
